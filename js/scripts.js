88764285ea377dcb55b3604ad537475dc5d31939
noribang <hoonk9387@gmail.com>
2017-06-03 23:57:24 -0500

    add jquery so alert() dialog box only opens once per prepended <li>

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
 	$('button#hello').click(function() {
 		$('ul#user').prepend('<li>Hello!</li>');
 		$('ul#webpage').prepend('<li>Why hello there!</li>');
-		$('li').click(function() {
-			alert('hi');	
+		$('ul#user').children('li').first().click(function() {
+			alert('hi');
 		});
-	});
+		$('ul#webpage').children('li').first().click(function() {
+			alert('why hi');
+		});
+	});// Button Hello
 	$('button#goodbye').click(function() {
 		$('ul#user').prepend('<li>Goodbye</li>');
 		$('ul#webpage').prepend('<li>Goodbye, dear user!</li>');
